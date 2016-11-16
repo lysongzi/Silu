c50faea24dae7a0f156dfde6be428e37d639a128
lysongzi <lysongzi@163.com>
2016-11-16 21:08:45 +0800

    logo调整

--- NewTry/LYSFirstViewController.m
+++ NewTry/LYSFirstViewController.m
@@ -16,6 +16,7 @@
 @property (weak, nonatomic) IBOutlet UIImageView *dateimage;
 
 @property (weak, nonatomic) IBOutlet UIView *sline;
+@property (assign, nonatomic) NSInteger index;
 
 @end
 
@@ -69,26 +70,57 @@
     CGFloat height = self.scrollView.contentSize.height;
     CGFloat nowY = self.scrollView.contentOffset.y;
     CGFloat divide = nowY / height;
-    int index = 0;
-    if (divide >= 5/6.0) {
-        index = 5;
+
+    BOOL change = NO;
+    if (divide >= 5/6.0-0.05) {
+        if (self.index != 5) {
+            change = YES;
+        }
+        self.index = 5;
     }
     else if (divide >= 4/6.0){
-        index = 4;
+        if (self.index != 4) {
+            change = YES;
+        }
+        self.index = 4;
     }
     else if (divide >= 3/6.0){
-        index = 3;
+        if (self.index != 3) {
+            change = YES;
+        }
+        self.index = 3;
     }
     else if (divide >= 2/6.0){
-        index = 2;
+        if (self.index != 2) {
+            change = YES;
+        }
+        self.index = 2;
     }
     else if (divide >= 1/6.0){
-        index = 1;
+        if (self.index != 1) {
+            change = YES;
+        }
+        self.index = 1;
     }
     else{
-        index = 0;
+        if (self.index != 0) {
+            change = YES;
+        }
+        self.index = 0;
     }
-    self.dateimage.image = [UIImage imageNamed:[NSString stringWithFormat:@"date%d", index + 1]];
+    
+    self.dateimage.image = [UIImage imageNamed:[NSString stringWithFormat:@"date%zi", self.index + 1]];
+//    if (change) {
+//        [UIView animateWithDuration:0.3 animations:^{
+//            self.dateimage.alpha = .0f;
+//        } completion:^(BOOL finished) {
+//            
+//            [UIView animateWithDuration:0.3 animations:^{
+//                self.dateimage.alpha = 1.0f;
+//            }];
+//        }];
+//    }
+    
 }
 
 @end
